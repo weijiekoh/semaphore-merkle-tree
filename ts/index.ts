c62d4f853c04ea888d9399de55d69c7b86096580
Koh Wei Jie <contact@kohweijie.com>
2019-07-26 14:54:57 +0800

    used export = instead of export default

--- ts/index.ts
+++ ts/index.ts
@@ -2,7 +2,7 @@ import { IStorage, MemStorage } from './storage'
 import { IHasher, Mimc7Hasher, MimcSpongeHasher } from './hashers'
 import MerkleTree from './merkletree'
 
-export default {
+export = {
 	storage: {
 		MemStorage,
 	},
